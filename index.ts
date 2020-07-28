0ac081662d548aa5fac57d7e3414fd0d9378b051
Biobots <biobot@qq.com>
2020-07-28 20:02:17 +0800

    use tab
    Signed-off-by: Biobots <biobot@qq.com>

--- index.ts
+++ index.ts
@@ -13,25 +13,25 @@ app.get('/', function(req:express.Request, res:express.Response) {
 	let ret:any = 'fail'
 	
 	//proxies
-    let proxies:Array<Proxy.BaseProxy>=[];
+	let proxies:Array<Proxy.BaseProxy>=[];
 	let promises:Array<Promise<AxiosResponse<any>>>=[]
 	Config.proxyurl.forEach(url => promises.push(Axios.get(url)));
-    Promise.all(promises)
-        .then(res => {
-            proxies = res.map(item => item.data)
-                .map(doc => <any>yaml.safeLoad(doc))
-                .map(obj => <Array<any>>obj.proxies)
-                .map(raw => Parser.parseProxies(raw))
-                .reduce((all, cur) => all.concat(cur))
-        })
-        .finally(() => {
-            proxies = Parser.filterBySrcLoc(proxies, '香港');
+	Promise.all(promises)
+		.then(res => {
+			proxies = res.map(item => item.data)
+				.map(doc => <any>yaml.safeLoad(doc))
+				.map(obj => <Array<any>>obj.proxies)
+				.map(raw => Parser.parseProxies(raw))
+				.reduce((all, cur) => all.concat(cur))
+		})
+		.finally(() => {
+			proxies = Parser.filterBySrcLoc(proxies, '香港');
 			ret = proxies;
-            res.send(ret);
+			res.send(ret);
 		})
 });
 
 app.listen(1234, function() {
-    console.log('running');
-    Config.load('config.yml');
+	console.log('running');
+	Config.load('config.yml');
 });
--- utils/rule.ts
+++ utils/rule.ts
@@ -1,22 +1,22 @@
 export class PayloadRule {
-    type:string
-    content:string
+	type:string
+	content:string
 
-    constructor(raw:string) {
-        let str:Array<string> = raw.split(',');
-        this.type = str[0];
-        this.content = str[1];
-    }
+	constructor(raw:string) {
+		let str:Array<string> = raw.split(',');
+		this.type = str[0];
+		this.content = str[1];
+	}
 }
 
 export class SingleRule {
-    type:string
-    content:string
-    strategy:string
+	type:string
+	content:string
+	strategy:string
 
     constructor(type:string, content:string, strategy:string) {
-        this.type = type;
-        this.content = content;
-        this.strategy = strategy;
-    }
+		this.type = type;
+		this.content = content;
+		this.strategy = strategy;
+	}
 }
